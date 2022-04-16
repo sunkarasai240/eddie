f19616a2cddad286a7c3f91b7f2d5bcbfb0dfb07
sunkarasai240 <sunkarasai240@gmail.com>
2022-04-16 17:01:17 +0530

    vertical

---
+++ artifact/src/main/java/tutorial1/App.java
@@ -0,0 +1,13 @@
+package tutorial1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
